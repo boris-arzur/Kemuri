92bcf43f85a3b77c59b27039250bc9edf45c1373
Boris Arzur <boris.arzur@jp.amundi.com>
2015-12-14 12:16:48 +0900

    Cleanup dead code

--- servlets.rb
+++ servlets.rb
@@ -21,81 +21,10 @@ require 'socket'
 require 'yaml'
 require 'zlib'
 
-#if RUBY_VERSION == '1.9.0'
-#  #print Encoding.default_external
-#elsif RUBY_VERSION > '1.9.0'
-#  Encoding.default_external="UTF-8"
-#  Encoding.default_internal="UTF-8"
-#else
-#  $KCODE = 'u' # pour le split //
-#end
-
-#def plog *t
-#  log t.map{|i| i.inspect}.join("\n")
-#end
-#
-#$log_mx = Mutex.new
-#$log_bf = []
-#
-#$history_mx = Mutex.new
-#$history_bf = []
-#
-#def log(*t)
-#  t = t[0] if t.size == 1
-#  $log_mx.synchronize do $log_bf << "[@#{Time.new.to_i.to_s(36)}] #{t}" end
-#end
-#
-#def to_history(t)
-#  $history_mx.synchronize do $history_bf << t end
-#end
-#
-#def message(m)
-#  $me.message(m)
-#end
-
 require './engine.rb'
 require './dbs.rb'
 require './static.rb'
 require './kanji_table.rb'
-
-#class Server
-#  def initialize()
-#    @port = $kemuri_port || 8185
-#    @name = 'Kemuri'
-#    @listen = $kemuri_bind || '127.0.0.1'
-#
-#    @listener = ::TCPServer.new(@listen, @port)
-#    @message_mutex = Mutex.new
-#    @message = false
-#    log('Running on Ruby ' + RUBY_VERSION)
-#    log("Bound to #{@listen}:#{@port}.")
-#  end
-#
-#          @message_mutex.synchronize do
-#            if @message
-#              answer = "<div style='color:red'>#{@message}</div>" + answer
-#              @message = false
-#            end
-#          end
-#
-#
-#  def to_url
-#      raise 
-#    "/"
-#  end
-#
-#  def message(m)
-#      raise 
-#    m = "/!\\ #{m} /!\\"
-#    @message_mutex.synchronize do
-#      if @message
-#        @message += "<br/>#{m}"
-#      else
-#        @message = m
-#      end
-#    end
-#  end
-#end
 
 class Servlets
   @@servlets = {}
@@ -119,12 +48,3 @@ class Servlets
     @@servlets[path[0]].execute(request, path, query, response)
   end
 end
-
-#def server_start
-#  `rm server.log;touch server.log`
-#  Servlet::register_all
-#  $me = Server.new
-#  File.open("kemuri.pid", "w") {|f| f.print(Process.pid)}
-#  $me.start_flusher
-#  $me.start_serving
-#end
